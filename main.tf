8c9d7fde42a1bc965d62c68cc5f0288fa95554d4
Batman-22 <162820542+Batman-22@users.noreply.github.com>
2024-09-07 18:39:43 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,7 +5,7 @@ resource "aws_s3_bucket" "s3" {
 
 resource "aws_instance" "myins" {
     ami = "ami-0ad21ae1d0696ad58"
-    instance_type = "t2.micro"
+    instance_type = "t2.medium"
     subnet_id = aws_subnet.newsub.id
     key_name = "jenkins-server-key"
     associate_public_ip_address = true
