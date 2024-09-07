75d76b4c6d251b45b7c20ec046e8caa9ed9ce996
Batman-22 <162820542+Batman-22@users.noreply.github.com>
2024-09-07 18:51:07 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "myins" {
-    ami = "ami-0ad21ae1d0696ad58"
+    ami = "ami-0522ab6e1ddcc7055"
     instance_type = "t2.medium"
     subnet_id = aws_subnet.newsub.id
     key_name = "jenkins-server-key"
